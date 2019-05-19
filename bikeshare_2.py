93729e0b0d2f904c68003eff7ab6ebcea29f4ac7
Dilek Savas <dileksavas03@gmail.com>
2019-05-19 22:46:16 +0200

    project started day added

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -127,6 +127,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
+        print ("The project started on the 12th January")
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
