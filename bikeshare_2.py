182bf47a4519f4d0938714e53fae0d52f659f8d2
Dilek Savas <dileksavas03@gmail.com>
2019-05-19 22:48:05 +0200

    project finished day added

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -128,7 +128,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         print ("The project started on the 12th January")
-
+        print ("The project finished on the 21st May")
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
